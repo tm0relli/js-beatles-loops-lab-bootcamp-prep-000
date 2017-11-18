b0fb30f8b626d8e0801e25ee66c604d5366c8a2b
Theresa <theresa.l.morelli@gmail.com>
2017-11-18 03:45:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicianArray, instrumentArray) {
   var newArray = [];
   for (var m = 0; m < musicianArray.length; m++) {
-    newArray.push(`${musicianArray[m]} plays ${instrumentArray[m]}.`);
+    newArray.push(`${musicianArray[m]} plays ${instrumentArray[m]}`);
   }
   return newArray;
 }
