6b640d97c0387b4fe5d8e56127ceab8f6295cf68
Theresa <theresa.l.morelli@gmail.com>
2017-11-18 04:10:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,13 @@ function johnLennonFacts(arrayOfFacts) {
   }
   return newArray;
 }
+
+
+function iLoveTheBeatles(num) {
+  var array = [];
+  do {
+    array.push('I love the Beatles!');
+    num++;
+  } while (num < 15);
+  return array;
+}
